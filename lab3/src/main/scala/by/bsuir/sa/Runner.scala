db4541e3ad04ff19f7cc2c2bf584d39de1e6f2b8
Ivan Taleika <ivan.taleika@gmail.com>
2019-04-02 12:41:33 +0300

    Added lab3 Pearson method test

--- lab3/src/main/scala/by/bsuir/sa/Runner.scala
+++ lab3/src/main/scala/by/bsuir/sa/Runner.scala
@@ -1,36 +1,70 @@
 package by.bsuir.sa
 
+import breeze.stats.distributions._
 import com.sksamuel.scrimage.Image
 import com.sksamuel.scrimage.filter.GrayscaleFilter
 import org.apache.spark.mllib.linalg.Vectors
 import org.apache.spark.mllib.stat.Statistics
+import org.jfree.chart.plot.CombinedDomainXYPlot
+import scalax.chart._
 import scalax.chart.api._
-object Runner extends App {
-  val imageUrl = "image.jpg"
-//  Image
-//    .fromStream(getClass.getResourceAsStream(imageUrl))
-//    .filter(GrayscaleFilter).output("birdGrey.jpg")
-  val histogram = Image
-    .fromStream(getClass.getResourceAsStream(imageUrl))
-    .filter(GrayscaleFilter)
-    .pixels
-    .map(_.red)
-    .groupBy(_ / 10)
-    .map(t => (t._1, t._2.length))
 
-  val distribution = Vectors.dense(histogram.values.map(_.toDouble).toArray)
-  val chi2Result = Statistics.chiSqTest(distribution)
+object Runner extends App {
+
+//  println("Normal distribution test")
+//  val normalDistrPlot = testImageDistribution("image", Gaussian(12.5, 3))
+//  println("\nF-distribution test")
+
+  val fDistrPlot = testImageDistribution("fDistr", LogNormal(2, 1))
+  val plot = new CombinedDomainXYPlot()
+//  plot.add(normalDistrPlot)
+  plot.add(fDistrPlot)
+  XYChart(plot, "histogram", legend = false)(ChartTheme.Default).show("histogram")
+//  val spark = SparkSession.builder().master("local[*]").getOrCreate()
+
+//  val ksResult = Statistics.kolmogorovSmirnovTest(
+//    spark.sparkContext.parallelize(expectedValues.map(_._2)),
+//    myCdf)
 
 //  val ksResult = Statistics.kolmogorovSmirnovTest(distribution., "norm")
-  println(chi2Result)
-//  println("--------------------------------------")
-//  println(
-//    Statistics.chiSqTest(
-//      Vectors.dense(StudentsT(25).sample(26).map(abs(_)).toArray)))
 
-  val chart = XYBarChart(histogram.toList)
-  chart.plot.getDomainAxis.setLabel("bucket")
-  chart.plot.getRangeAxis.setLabel("number of pixels")
+//  println(ksResult)
 
-  chart.show("histogram")
+  def testImageDistribution(
+      imageName: String,
+      expectedDistribution: ContinuousDistr[Double] with HasCdf)
+    : CombinedDomainXYPlot = {
+    val image = Image
+      .fromStream(getClass.getResourceAsStream(s"$imageName.jpg"))
+      .filter(GrayscaleFilter)
+    image.output(s"${imageName}Grey.jpg")
+
+    val expectedValues =
+      (0 to 25).map(i => (i, expectedDistribution.probability(i, i + 1)))
+    val histogram = createColorHistogram(image)
+
+    val chi2Result =
+      Statistics.chiSqTest(Vectors.dense(histogram.map(_._2).toArray),
+                           Vectors.dense(expectedValues.map(_._2).toArray))
+
+    println(chi2Result)
+    createCombinedPlot(expectedValues, histogram)
+  }
+
+  def createColorHistogram(image: Image): Seq[(Int, Double)] = {
+    image.pixels
+      .map(_.red)
+      .groupBy(_ / 10)
+      .map(t => (t._1, t._2.length.toDouble / image.pixels.length))
+      .toList
+      .sortBy(_._1)
+  }
+
+  def createCombinedPlot(s1: Seq[(Int, Double)],
+                         s2: Seq[(Int, Double)]): CombinedDomainXYPlot = {
+    val plot = new CombinedDomainXYPlot()
+    plot.add(XYBarChart(s1).plot)
+    plot.add(XYBarChart(s2).plot)
+    plot
+  }
 }
