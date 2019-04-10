5a96a4e2b8d3efafb830f34501b32b1b4249ae53
Ivan Taleika <xefzar@gmail.com>
2019-04-11 02:52:09 +0300

    Created word count modeal

---
+++ lab4/src/main/scala/by/bsuir/sa/CleanerModelFactory.scala
@@ -0,0 +1,42 @@
+package by.bsuir.sa
+
+import com.johnsnowlabs.nlp.annotators.{Normalizer, Stemmer, Tokenizer}
+import com.johnsnowlabs.nlp.{DocumentAssembler, Finisher}
+import org.apache.spark.ml.feature.StopWordsRemover
+import org.apache.spark.ml.{Pipeline, PipelineModel}
+import org.apache.spark.sql.SparkSession
+
+object CleanerModelFactory {
+  private val documentAssembler =
+    new DocumentAssembler().setInputCol("text").setOutputCol("document")
+  private val tokenizer =
+    new Tokenizer()
+      .setInputCols("document")
+      .setOutputCol("tokened")
+  private val normalizer =
+    new Normalizer().setInputCols("tokened").setOutputCol("normalized")
+  private val stemmer = new Stemmer()
+    .setInputCols("normalized")
+    .setOutputCol("stemmed")
+  private val finisher = new Finisher()
+    .setInputCols("stemmed")
+    .setOutputCols("finished")
+  private val cleaner = new StopWordsRemover()
+    .setInputCol("finished")
+    .setOutputCol("result")
+  cleaner.setStopWords(cleaner.getStopWords ++ Array("", "ar", "thi", "wa", "hi", "thei", "u", "us"))
+  private val pipeline: Pipeline = new Pipeline()
+    .setStages(
+      Array(documentAssembler,
+        tokenizer,
+        normalizer,
+        stemmer,
+        finisher,
+        cleaner))
+
+  def model(spark: SparkSession): PipelineModel = {
+    import spark.implicits._
+    pipeline.fit(spark.createDataset(Seq.empty[String]).toDF("text"))
+  }
+
+}
--- lab4/src/main/scala/by/bsuir/sa/Runner.scala
+++ lab4/src/main/scala/by/bsuir/sa/Runner.scala
@@ -1,73 +1,34 @@
 package by.bsuir.sa
 
-import com.johnsnowlabs.nlp.annotators.{Normalizer, Stemmer, Tokenizer}
-import com.johnsnowlabs.nlp.{DocumentAssembler, Finisher}
-import org.apache.spark.ml.Pipeline
-import org.apache.spark.ml.feature.{CountVectorizer, StopWordsRemover}
-import org.apache.spark.sql.SparkSession
+import org.apache.spark.ml.feature.CountVectorizer
+import org.apache.spark.sql.{DataFrame, SparkSession}
 
-// TODO: remove empty words
-// TODO: remove stop words before stamming
 object Runner extends App {
   val spark =
     SparkSession.builder().master("local[*]").appName("lab4").getOrCreate()
   spark.sparkContext.setLogLevel("WARN")
-  val spaceDf =
-    spark
-      .createDataFrame(
-        (0 to 3).map(
-          i =>
-            (i,
-              new String(
-                this.getClass.getResourceAsStream(s"/space/$i").readAllBytes()))))
-      .toDF("index", "text")
-  import spark.implicits._
-
-  val documentAssembler =
-    new DocumentAssembler().setInputCol("text").setOutputCol("document")
-  val tokenizer =
-    new Tokenizer()
-      .setInputCols("document")
-      .setOutputCol("tokens")
-
-  val normalizer =
-    new Normalizer().setInputCols("tokens").setOutputCol("normalized")
-
-  val stemmer = new Stemmer()
-    .setInputCols("normalized")
-    .setOutputCol("stemms")
-
-  val finisher = new Finisher()
-    .setInputCols("stemms")
-    .setOutputCols("result")
-
-  val remover = new StopWordsRemover()
-    .setInputCol("result")
-    .setOutputCol("filtered")
-  val pipeline = new Pipeline()
-    .setStages(
-      Array(documentAssembler,
-            tokenizer,
-            normalizer,
-            stemmer,
-            finisher,
-            remover))
-    .fit(
-      spark
-        .createDataset(Seq(new String(
-          this.getClass.getResourceAsStream("/space/0").readAllBytes())))
-        .toDF("text"))
-  //  new Pipeline().setStages(Array(tokenizer, stemmer)).fit()
-
-
-  val temp = pipeline.transform(spaceDf)
-  temp.show(true)
+  val cleanerModel = CleanerModelFactory.model(spark)
   val vectorizer = new CountVectorizer()
-    .setInputCol("filtered")
+    .setInputCol("result")
     .setOutputCol("features")
     .setVocabSize(10)
     .setMinDF(1)
-  val model = vectorizer.fit(temp)
-  println(model.vocabulary.mkString(" "))
-  model.transform(temp).select("filtered", "features").show(false)
+
+  val spaceModel = vectorizer.fit(cleanerModel.transform(createDf("space", 3)))
+  val christianModel = vectorizer.fit(cleanerModel.transform(createDf("christian", 3)))
+  val cryptographyModel = vectorizer.fit(cleanerModel.transform(createDf("cryptography", 3)))
+  println(spaceModel.vocabulary.mkString(" "))
+  spaceModel.transform(cleanerModel.transform(createDf("space", 9))).select("features").show(false)
+
+
+  def createDf(folder: String, n: Int): DataFrame = {
+    spark
+      .createDataFrame(
+        (0 to n).map(
+          i =>
+            (i,
+              new String(
+                this.getClass.getResourceAsStream(s"/$folder/$i").readAllBytes()))))
+      .toDF("index", "text")
+  }
 }
