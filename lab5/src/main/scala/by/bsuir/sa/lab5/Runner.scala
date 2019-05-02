894486b0ef7747b028f22db3a873093be106fa4a
Ivan Taleika <xefzar@gmail.com>
2019-05-03 00:38:16 +0300

    Reorganized structure

--- lab5/src/main/scala/by/bsuir/sa/lab5/Runner.scala
+++ lab5/src/main/scala/by/bsuir/sa/lab5/Runner.scala
@@ -9,58 +9,94 @@ import scala.util.Random
 
 object Runner extends App {
 
-  val data = Source
-    .fromResource("kaggle_datasets_train.csv")
-    .getLines()
-    .map(_.split(""",(?=([^\"]*\"[^\"]*\")*[^\"]*$)"""))
-  // kernels, discussions, views, downloads, size, featured, super_featured, upvotes
-  val labels = data
-    .map(a => {
-      val temp = a.slice(4, a.length)
-      temp.take(4).map(_.toDouble)
-//      ++ temp.takeRight(4).map(_.toDouble)
-    })
-    .toList
+  val Array(mode, path) = args
+  mode match {
+    case "train" => train(extractData(path))
+    case "test"  => test(extractWeights(), extractData(path))
+  }
 
-  val N = labels.head.length
+  def test(neurons: Seq[Seq[Double]], data: Seq[Seq[Double]]): Unit = {
+    println("TODO")
+  }
 
-  val columnCoeffs =
-    (0 until N).map { i =>
-      val maxMin = (labels.maxBy(_(i))(implicitly[Ordering[Double]])(i),
-                    labels.minBy(_(i))(implicitly[Ordering[Double]])(i))
-      (calcA(maxMin), calcB(maxMin))
+  def extractWeights(): Seq[Seq[Double]] = {
+    Source
+      .fromFile("weights/weights.csv")
+      .getLines()
+      .map(_.split(",").map(_.toDouble).toSeq)
+      .toSeq
+  }
+
+  def train(data: Seq[Seq[Double]]): Unit = {
+
+    val normalizationCoeffs = calcNormalizationCoeffs(data)
+    val normalizedData = data.map(r =>
+      r.indices.map(i =>
+        r(i) * normalizationCoeffs(i)._1 + normalizationCoeffs(i)._2))
+    val neurons = trainNeurons(normalizedData)
+
+    val denormalizedNeurons = neurons.map(ws =>
+      ws.indices.map(i =>
+        (ws(i) - normalizationCoeffs(i)._2) / normalizationCoeffs(i)._1))
+
+    Files.write(Paths.get("weights/weights.csv"),
+                denormalizedNeurons
+                  .map(_.mkString(","))
+                  .mkString("\n")
+                  .getBytes(StandardCharsets.UTF_8))
+
+  }
+
+  def trainNeurons(data: Seq[Seq[Double]]): Seq[Seq[Double]] = {
+    val Speed = 0.3
+    val Step = -0.05
+    // 4 neurons, for example
+    val M = 4
+
+    var neurons =
+      (0 until M).map(_ =>
+        data.head.indices.map(_ => Random.nextDouble() * 0.2 + 0.1))
+
+    BigDecimal(Speed).until(0).by(Step).foreach { speed =>
+      data.foreach { xs =>
+        val i =
+          neurons
+            .map(vectorsDistance(xs, _))
+            .zipWithIndex
+            .maxBy(_._1)
+            ._2
+        neurons =
+          neurons.updated(i, weightUpdate(neurons(i), xs, speed.toDouble))
+      }
     }
+    neurons
+  }
 
-  val normalizedLabels = labels.map(r =>
-    r.indices.map(i => r(i) * columnCoeffs(i)._1 + columnCoeffs(i)._2))
+  def extractData(paths: String): Seq[Seq[Double]] = {
+    val data = Source
+      .fromResource(path)
+      .getLines()
+      .map(_.split(""",(?=([^\"]*\"[^\"]*\")*[^\"]*$)"""))
 
-  // 4 neurons, for example
-  var neurons =
-    (0 to 3).map(_ => (0 until N).map(_ => Random.nextDouble() * 0.2 + 0.1))
+    // kernels, discussions, views, downloads, size, featured, super_featured, upvotes
+    data
+      .map(a => {
+        val temp = a.slice(4, a.length)
+        (temp
+          .take(4)
+          .map(_.toDouble) ++ temp.takeRight(4).map(_.toDouble)).toList
+      })
+      .toList
+  }
 
-  val l = 0.3
-  val step = -0.05
-
-  BigDecimal(l).until(0).by(step).foreach { speed =>
-    normalizedLabels.foreach { xs =>
-      val i =
-        neurons
-          .map(vectorsDistance(xs, _))
-          .zipWithIndex
-          .maxBy(_._1)
-          ._2
-      neurons = neurons.updated(i, weightUpdate(neurons(i), xs, speed.toDouble))
+  def calcNormalizationCoeffs(data: Seq[Seq[Double]]): Seq[(Double, Double)] = {
+    data.head.indices.map { i =>
+      val maxMin = (data.maxBy(_(i))(implicitly[Ordering[Double]])(i),
+                    data.minBy(_(i))(implicitly[Ordering[Double]])(i))
+      (calcA(maxMin), calcB(maxMin))
     }
   }
 
- val denormalizedNeurons = neurons.map(ws =>
-    ws.indices.map(i =>( ws(i) - columnCoeffs(i)._2) / columnCoeffs(i)._1))
-  Files.write(Paths.get("weights.csv"),
-    denormalizedNeurons
-                .map(_.mkString(","))
-                .mkString("\n")
-                .getBytes(StandardCharsets.UTF_8))
-//
   def calcA(maxMin: (Double, Double)): Double = {
     1d / (maxMin._1 - maxMin._2)
   }
@@ -75,6 +111,7 @@ object Runner extends App {
 
   def weightUpdate(ws: Seq[Double],
                    xs: Seq[Double],
-                   l: Double): collection.immutable.IndexedSeq[Double] =
-    ws.indices.map(i => ws(i) + l * (xs(i) - ws(i)))
+                   L: Double): collection.immutable.IndexedSeq[Double] =
+    ws.indices.map(i => ws(i) + L * (xs(i) - ws(i)))
+
 }
